a24b2999dcf16759f5aad907935ebd8aac6007ad
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2022-12-20 16:00:40 -0300

    footer responsive

--- src/pages/landingPage1/index.js
+++ src/pages/landingPage1/index.js
@@ -7,7 +7,6 @@ import { isBrowser, isMobile } from 'react-device-detect';
 import styles from './landingPage1.module.scss';
 import Header from '../../components/header';
 import SearchInput from '../../components/searchInput';
-import BlueButton from '../../components/blueButton';
 import Footer from '../../components/footer';
 import TransparentButton from '../../components/transparentButton';
 import AgentTestimonial from '../../components/agentTestimonial';
@@ -24,6 +23,7 @@ import ListingIcon from '../../assets/images/listingIcon.svg';
 import GetSmartedIcon from '../../assets/images/getSmartedIcon.svg';
 import PickWinnerIcon from '../../assets/images/pickWinnerIcon.svg';
 import Journals from '../../assets/images/journals.svg';
+import JournalsWeb from '../../assets/images/journalsWeb.svg';
 import PriceIcon from '../../assets/images/iconPrice.svg';
 import GetPaidIcon from '../../assets/images/getPaidIcon.svg';
 import Arrow from '../../assets/images/arrow.svg';
@@ -100,7 +100,7 @@ function LandingPage() {
                 <div className={styles.customerReviews}>
                   <div className={styles.customerReviewsTitle}>
                     <span>
-                      <img src={Stars} alt="Stars review" />
+                      <Image src={Stars} alt="Stars review" />
                     </span>
                     <span className={styles.reviewNumber}>
                       4.8
@@ -108,7 +108,7 @@ function LandingPage() {
                   </div>
                   <div className={styles.customerReviewsText}>
                     <span>
-                      <img src={GoogleIcon} alt="Google icon" />
+                      <Image src={GoogleIcon} alt="Google icon" />
                     </span>
                     <span>
                       Average of 290+ customer reviews
@@ -225,48 +225,55 @@ function LandingPage() {
         <section id={styles.findPerfectAgent}>
           <Container className="mt-5 p-3">
             <h3>Get Paid Upfront to Find Your Perfect Agent</h3>
-            <div className="d-flex justify-content-center align-items-center">
-              <img src={HouseListing} alt="Get Paid Upfront to Find Your Perfect Agent" />
-            </div>
-            <div className="d-flex flex-direction-row align-items-center pt-2">
-              <span className="d-flex align-items-center">
-                <img height="22px" src={PriceIcon} alt="Get Paid Upfront" />
-              </span>
-              <div className={styles.findPerfectAgentTitle}>
-                <span className="px-2">
-                  $1,440
-                </span>
-              </div>
-            </div>
-            <div>
-              <span className={styles.textMuted}>Homeowner Received at List</span>
-            </div>
-            <hr />
-            <div className="d-flex flex-column">
-              <span className={styles.findPerfectAgentSubTitle}>10 Bids</span>
-              <span className={styles.textMuted}>Received</span>
-            </div>
-            <hr />
-            <div className="d-flex flex-column">
-              <span className={styles.findPerfectAgentSubTitle}>$589,000</span>
-              <span className={styles.textMuted}>Listed for</span>
-              <hr />
-            </div>
-            <TransparentButton text="Next Listing" icon={ArrowRightIcon} />
+            <Row>
+              <Col md={8} xs={12}>
+                <div className="d-flex justify-content-center align-items-center">
+                  <Image
+                    className={styles.findPerfectAgentImage}
+                    src={HouseListing}
+                    alt="Get Paid Upfront to Find Your Perfect Agent"
+                  />
+                </div>
+              </Col>
+              <Col md={4} xs={12} className={styles.getPaidUpFront}>
+                <div className="d-flex flex-direction-row align-items-center pt-2">
+                  <span className="d-flex align-items-center">
+                    <Image height="22px" src={PriceIcon} alt="Get Paid Upfront" />
+                  </span>
+                  <div className={styles.findPerfectAgentTitle}>
+                    <span className="px-2">
+                      $1,440
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <span className={styles.textMuted}>Homeowner Received at List</span>
+                </div>
+                <hr />
+                <div className="d-flex flex-column">
+                  <span className={styles.findPerfectAgentSubTitle}>10 Bids</span>
+                  <span className={styles.textMuted}>Received</span>
+                </div>
+                <hr />
+                <div className="d-flex flex-column">
+                  <span className={styles.findPerfectAgentSubTitle}>$589,000</span>
+                  <span className={styles.textMuted}>Listed for</span>
+                  <hr />
+                </div>
+                <TransparentButton text="Next Listing" icon={ArrowRightIcon} />
+              </Col>
+            </Row>
           </Container>
         </section>
 
         <section id={styles.findAgentNextYou}>
-          <Container>
+          <Container className={styles.findAgentNextYouContainer}>
             <div className="mt-3 d-flex align-items-center justify-content-center pt-3">
-              <h3 className="text-center pt-2">Find a Top Real Estate Agent Near You</h3>
+              <h3>Find a Top Real Estate Agent Near You</h3>
             </div>
             <Image src={Arrow} alt="Enter your home address" />
             <div className="mx-2">
               <SearchInput icon={MapIcon} text="Enter your home address" />
-            </div>
-            <div className="mx-2 mt-2">
-              <BlueButton text="Let's go" />
             </div>
           </Container>
         </section>
@@ -275,7 +282,7 @@ function LandingPage() {
           <Container className="mt-5">
             <h3 className="text-center">As Seen In</h3>
             <div className="mt-4">
-              <Image src={Journals} alt="As Seen In" />
+              <Image src={isMobile ? Journals : JournalsWeb} alt="As Seen In" />
             </div>
           </Container>
         </section>
